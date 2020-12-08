fb4014c792e79bede74ea8c900475f3ccf0b3f5f
yadhu <adamyadhu123@gmail.com>
2020-12-08 11:56:07 +0530

    commit3

--- Home.js
+++ Home.js
@@ -1,10 +1,28 @@
-var btn2 = document.getElementById("btn2");
-btn2.addEventListener("click",function(){
+ function display(){
     var xhttp = new XMLHttpRequest();
     xhttp.open('GET','https://jsonplaceholder.typicode.com/todos');
     xhttp.onload=function(){
         var data = JSON.parse(xhttp.responseText);
-        console.log(data[0]);
     };
     xhttp.send();
-});
+};
+
+// binding
+
+var databind= function(){
+    var dataitem = databind.bind(func);
+    var tableData=data;
+
+    for (var i in tableData){
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                    <td>${tableData[i].Userid}</td>
+                    <td>${tableData[i].id}</td>
+                    <td>${tableData[i].title}</td>
+                    <td>${tableData[i].Completed}</td>
+                      `
+          var table = $('#table-body')
+        table.append(row)
+      }
+    
+}
