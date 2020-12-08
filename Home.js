63c5a5a5e1fab3efb7d3fdddac797679f9ab0fc7
yadhu <adamyadhu123@gmail.com>
2020-12-08 12:52:42 +0530

    commit4

--- Home.js
+++ Home.js
@@ -1,28 +1,21 @@
- function display(){
+function display(){
     var xhttp = new XMLHttpRequest();
     xhttp.open('GET','https://jsonplaceholder.typicode.com/todos');
     xhttp.onload=function(){
-        var data = JSON.parse(xhttp.responseText);
+    var data = JSON.parse(xhttp.responseText);
+    for (var i in data){
+        //Keep in mind we are using "Template Litterals to create rows"
+        var row = `<tr>
+                    <td>${data[i].Userid}</td>
+                    <td>${data[i].id}</td>
+                    <td>${data[i].title}</td>
+                    <td>${data[i].Completed}</td>
+                     </tr> `
+          var table = $('#table-body')
+        table.append(row)
+      }
     };
     xhttp.send();
 };
 
-// binding
 
-var databind= function(){
-    var dataitem = databind.bind(func);
-    var tableData=data;
-
-    for (var i in tableData){
-        //Keep in mind we are using "Template Litterals to create rows"
-        var row = `<tr>
-                    <td>${tableData[i].Userid}</td>
-                    <td>${tableData[i].id}</td>
-                    <td>${tableData[i].title}</td>
-                    <td>${tableData[i].Completed}</td>
-                      `
-          var table = $('#table-body')
-        table.append(row)
-      }
-    
-}
